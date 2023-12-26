cb610f6951ec27ad3816d22851ab326659532c75
Carlos Eduardo <dev.carloseddie@outlook.com>
2023-12-26 11:03:26 -0300

    created home page

---
+++ aluroni/src/pages/Home/index.tsx
@@ -0,0 +1,29 @@
+import menu from 'data/menu.json';
+import styles from './Home.module.scss';
+
+export default function Home() {
+
+    let recommendedDishes = [...menu];
+
+    recommendedDishes = recommendedDishes.sort(() => 0.5 - Math.random()).splice(0,3);
+
+    return (
+        <section>
+            <h3 className={styles.title}>
+                Kitchen recommendations
+            </h3>
+            <div className={styles.recommended2}>
+                {recommendedDishes.map(item => (
+                    <div key={item.id} className={styles.recommended1}>
+                        <div className={styles.recommended1__image}>
+                            <img src={item.photo} alt={item.title} />
+                        </div>
+                        <button className={styles.recommended1__button}>
+                            See more
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
