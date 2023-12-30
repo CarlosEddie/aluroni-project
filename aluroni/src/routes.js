e8fe4ea387028889aaba46e14814c73942fabb9d
Carlos Eduardo <dev.carloseddie@outlook.com>
2023-12-30 09:53:12 -0300

    created about page

--- aluroni/src/routes.js
+++ aluroni/src/routes.js
@@ -1,5 +1,6 @@
 import DefaultPage from 'Components/DefaultPage';
 import PageMenu from 'Components/PageMenu';
+import About from 'pages/About';
 import Home from 'pages/Home';
 import Menu from 'pages/Menu';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -13,6 +14,7 @@ export default function AppRouter() {
                     <Route path='/' element={<DefaultPage />}>
                         <Route index element={<Home />} />
                         <Route path='menu' element={<Menu />} />
+                        <Route path='about' element={<About />} />
                     </Route>
                 </Routes>
             </Router>
