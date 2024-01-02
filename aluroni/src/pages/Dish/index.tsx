fd66682640c9a30a84c021fdebc2beddec27dd60
Carlos Eduardo <dev.carloseddie@outlook.com>
2024-01-02 18:13:39 -0300

    refactored dish page

--- aluroni/src/pages/Dish/index.tsx
+++ aluroni/src/pages/Dish/index.tsx
@@ -1,8 +1,9 @@
 import styles from './Dish.module.scss';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Routes, Route } from 'react-router-dom';
 import menu from 'data/menu.json';
 import TagsDish from 'Components/TagsDish';
 import NotFound from 'pages/NotFound';
+import DefaultPage from 'Components/DefaultPage';
 
 export default function Dish() {
 
@@ -13,24 +14,30 @@ export default function Dish() {
         return <NotFound />;
     }
     return(
-        <>
-            <button className={styles.goBack} onClick={() => navigate(-1)}>
-                {'< Back'}
-            </button>
-            <section className={styles.container}>
-                <h1 className={styles.title}>
-                    {dish.title}
-                </h1>
-                <div className={styles.image}>
-                    <img src={dish.photo} alt={dish.title} />
-                </div>
-                <div className={styles.content}>
-                    <p className={styles.content__description}>
-                        {dish.description}
-                    </p>
-                    <TagsDish {...dish} />
-                </div>
-            </section>
-        </>
+        <Routes>
+            <Route path='*' element={ <DefaultPage /> }>
+                <Route index element={
+                    <>
+                        <button className={styles.goBack} onClick={() => navigate(-1)}>
+                            {'< Back'}
+                        </button>
+                        <section className={styles.container}>
+                            <h1 className={styles.title}>
+                                {dish.title}
+                            </h1>
+                            <div className={styles.image}>
+                                <img src={dish.photo} alt={dish.title} />
+                            </div>
+                            <div className={styles.content}>
+                                <p className={styles.content__description}>
+                                    {dish.description}
+                                </p>
+                                <TagsDish {...dish} />
+                            </div>
+                        </section>
+                    </>
+                } ></Route>
+            </Route>
+        </Routes>
     );
 }
